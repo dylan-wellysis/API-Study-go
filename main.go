7a5fa6d64445a3c5aaa23f5cc7b50180b56dc5da
dylan-wellysis <dylan.lee@wellysis.com>
2022-10-27 15:14:05 +0000

    comment POST,GET,DELETE 추가 - router를 gorilla mux로 대체 - 이에 따라 Handle() 대신 핸들러함수 직접 사용 - comment 구조체 및 관련된 구조체 추가 - MySQL DB에 comment Table 추가

--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 	"strings"
 	"time"
 
+	"github.com/gorilla/mux"
+
 	"github.com/gosimple/slug"
 
 	_ "github.com/go-sql-driver/mysql" // driver는 개발자가 직접 쓰지 않으므로, 언더바로 alias를 준다
@@ -18,29 +20,22 @@ var db *sql.DB // 전역변수로 설정 - 아직 더 나은 대안은 모르겠
 func main() {
 	var err error
 	db, err = sql.Open("mysql", "dylan:mysql@tcp(172.17.0.4:3306)/go_server?parseTime=true") // mysql is on another container, in same network "bridge"
-	if err != nil {
+	if err != nil || db.Ping() != nil {
 		fmt.Println(err)
 	}
 	defer db.Close()
 
-	// write any response with http.ResponseWriter
-	// handle any request with http.Request
-	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		w.Write([]byte("Hello World"))
-		fmt.Println(req.URL)
-	})
+	r := mux.NewRouter()
+	r.HandleFunc("/api/articles", handleArticle).Methods("POST")
+	r.HandleFunc("/api/articles/{slug}", handleArticle).Methods("GET")
+	r.HandleFunc("/api/articles/{slug}", handleArticle).Methods("PUT")
+	r.HandleFunc("/api/articles/{slug}", handleArticle).Methods("DELETE")
+	r.HandleFunc("/api/articles/{slug}/comments", handleComment).Methods("POST")
+	r.HandleFunc("/api/articles/{slug}/comments", handleComment).Methods("GET")
+	r.HandleFunc("/api/articles/{slug}/comments/{id}", handleComment).Methods("DELETE")
 
-	// 또 다른 HTTP handler 정의 방식 : http.Handle() 사용
-	// 두 번째 파라미터에 http.Handler 객체를 받음
-	http.Handle("/api/articles/", new(postHandler))
-
-	// In Go, nil is the zero value for pointers, interfaces, maps, slices, channels and function types, representing an uninitialized value.
-	// second arg: ServeMux. if nil, DefaultServeMux is set
-	http.ListenAndServe(":8080", nil)
-}
-
-type postHandler struct {
-	http.Handler
+	server := &http.Server{Addr: ":8080", Handler: r}
+	server.ListenAndServe()
 }
 
 // `json:something`와 같은 태깅으로 go에게 json을 인코딩/디코딩하는 방법을 알려준다
@@ -83,16 +78,30 @@ type ArticleResponseData struct {
 	Article ArticleRes `json:"article"`
 }
 
+type Comment struct {
+	Id        int       `json:"id"`
+	Body      string    `json:"body"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+	Author    Author    `json:"author"`
+}
+type CommentList struct {
+	Comments []Comment `json:"comments"`
+}
+type CommentRequest struct {
+	Body string `json:"body"`
+}
+type CommentRequestData struct {
+	Comment CommentRequest `json:"comment"`
+}
+
 func checkError(err error) {
 	if err != nil {
 		panic(err)
 	}
 }
 
-func Create(db *sql.DB) {}
-
-// TODO : response 하드코딩 목록 수정
-func (h *postHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+func handleArticle(w http.ResponseWriter, req *http.Request) {
 	fmt.Println("Method : ", req.Method)
 
 	// 주의: Body는 ReadCloser객체이므로, 두번 읽으면 사라지게 된다(EOF만 읽힘)
@@ -101,34 +110,21 @@ func (h *postHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	var err error
 	var reqData ArticleRequestData
 	var req_slug string
-	// var optionCheckMap map[string]interface{}
 
 	if req.Method == "POST" {
 		err = json.NewDecoder(req.Body).Decode(&reqData)
+		checkError(err)
 		defer req.Body.Close() // defer: 지연실행. 함수 종료 직전에 수행한다.
 		if err != nil {
 			fmt.Println(err)
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
-
-		// // POST에서는 옵션이 아닌 것들을 점검한다.
-		// // 이런 방법을 쓰기보단 method별로 기능을 구분해야할 것 같다.
-		// temp_data, _ := json.Marshal(reqData.Article)
-		// json.Unmarshal(temp_data, &optionCheckMap)
-		// _, ok := optionCheckMap["Desc"]
-		// if ok == false {
-		// 	http.Error(w, err.Error(), http.StatusBadRequest)
-		// 	return
-		// }
-		// _, ok = optionCheckMap["Body"]
-		// if ok == false {
-		// 	http.Error(w, err.Error(), http.StatusBadRequest)
-		// 	return
-		// }
 		req_slug = slug.Make(reqData.Article.Title)
 	} else {
-		req_slug = strings.TrimPrefix(req.URL.Path, "/api/articles/")
+		// req_slug = strings.TrimPrefix(req.URL.Path, "/api/articles/")
+		vars := mux.Vars(req)
+		req_slug = vars["slug"]
 		if len(req_slug) == 0 {
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
@@ -197,13 +193,13 @@ func (h *postHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 		article.TagList = strings.Split(temp_taglist, ",")
 		resData := &ArticleResponseData{Article: article}
 		json.NewEncoder(w).Encode(resData)
-		w.Write([]byte("single article"))
+		// w.Write([]byte("single article"))
 
 	// TODO: optional fields required
 	case "PUT":
 		var reqUpdateData ArticleUpdateData
 		err = json.NewDecoder(req.Body).Decode(&reqUpdateData)
-		defer req.Body.Close() // defer: 지연실행. 함수 종료 직전에 수행한다.
+		defer req.Body.Close()
 		if err != nil {
 			fmt.Println(err)
 			http.Error(w, err.Error(), http.StatusBadRequest)
@@ -215,5 +211,80 @@ func (h *postHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	case "DELETE":
 		_, err = db.Exec("DELETE FROM article WHERE id=?", article_id)
 		checkError(err)
+	}
+}
+
+func handleComment(w http.ResponseWriter, req *http.Request) {
+	fmt.Println("Method : ", req.Method)
+	vars := mux.Vars(req)
+	req_slug := vars["slug"]
+
+	// slug로 article 쿼리
+	// 복수의 rows를 받기 위해 Query()와 Next()를 사용
+	query := fmt.Sprintf("SELECT * FROM article WHERE slug='%s'", req_slug)
+	var article_id int
+	var article ArticleRes
+	var temp_taglist string
+	var temp_user_id int
+	err := db.QueryRow(query).Scan(&article_id, &article.Slug, &article.Title, &article.Desc, &article.Body, &temp_taglist, &article.CreatedAt, &article.UpdatedAt, &article.Favorited, &article.FavoritesCount, &temp_user_id)
+	checkError(err)
+
+	// article로 user 쿼리
+	query = fmt.Sprintf("SELECT * FROM realworld_user WHERE id=%d", temp_user_id)
+	var user_id int
+	var author Author
+	err = db.QueryRow(query).Scan(&user_id, &author.Username, &author.Bio, &author.Image, &author.Following)
+	checkError(err)
+
+	switch req.Method {
+	case "POST":
+		var reqData CommentRequestData
+		var resData Comment
+		err = json.NewDecoder(req.Body).Decode(&reqData)
+		checkError(err)
+		defer req.Body.Close()
+
+		resData.Id = article_id
+		resData.CreatedAt = time.Now()
+		resData.UpdatedAt = resData.CreatedAt // POST로 생성시에는 create time과 같다
+		resData.Body = reqData.Comment.Body
+		resData.Author = author
+		_, err = db.Exec("INSERT INTO comment VALUES (?,?,?,?,?,?)",
+			nil,
+			resData.CreatedAt,
+			resData.UpdatedAt,
+			resData.Body,
+			temp_user_id,
+			article_id,
+		)
+		checkError(err)
+		json.NewEncoder(w).Encode(resData)
+
+	case "GET":
+		var resData CommentList
+
+		// comment 쿼리
+		query = fmt.Sprintf("SELECT id, createdAt, updatedAt, body FROM comment WHERE article_id=%d", article_id)
+		rows, err := db.Query(query)
+		checkError(err)
+		defer rows.Close()
+
+		// query를 iterate하면서 comments를 받아놓음
+		for rows.Next() {
+			var comment Comment
+			comment.Author = author
+			err := rows.Scan(&comment.Id, &comment.CreatedAt, &comment.UpdatedAt, &comment.Body)
+			checkError(err)
+			resData.Comments = append(resData.Comments, comment)
+		}
+
+		json.NewEncoder(w).Encode(resData)
+
+	case "DELETE":
+		comment_id := vars["id"]
+		_, err = db.Exec("DELETE FROM comment WHERE id=?", comment_id)
+		checkError(err)
+
+		w.Write([]byte("delete completed"))
 	}
 }
